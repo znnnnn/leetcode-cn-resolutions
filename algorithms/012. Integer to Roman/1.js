b5c64f0909f57e079018b6b4553034d7e9f561ea
znnnnn <846179626@qq.com>
2019-01-26 19:32:58 +0800

    012. Integer to Roman

---
+++ algorithms/012. Integer to Roman/1.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  var values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+  reps = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+  for (let i = 0; i < 13; i++) {
+    console.log(reps[i]);
+  }
+};
