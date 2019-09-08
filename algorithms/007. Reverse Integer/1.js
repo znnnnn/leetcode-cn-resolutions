e871085679fc1c7a204cfb7c413d3d3fad7b6d19
znnnnn <846179626@qq.com>
2019-09-08 23:52:25 +0800

    update: format code

--- algorithms/007. Reverse Integer/1.js
+++ algorithms/007. Reverse Integer/1.js
@@ -2,15 +2,15 @@
  * @param {number} x
  * @return {number}
  */
-var reverse = function (x) {
+var reverse = function(x) {
   let rev = 0
   let bound = Math.pow(2, 31)
   while (x !== 0) {
-    rev = rev * 10 + x % 10
-    x = x / 10 | 0
+    rev = rev * 10 + (x % 10)
+    x = (x / 10) | 0
   }
   if (rev > bound - 1 || rev < -bound) {
-      return 0
+    return 0
   }
   return rev
-};
+}
