a87cb2e907f0ef407968724410397dd8edfc2550
znnnnn <846179626@qq.com>
2019-09-09 23:45:42 +0800

    update: format code

--- algorithms/008. String to Integer (atoi)/1.js
+++ algorithms/008. String to Integer (atoi)/1.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var myAtoi = function(str) {
-  var n = parseInt(str);
-  if(n){
-      if(n < -2147483648) return -2147483648;
-      if(n > 2147483647) return 2147483647;
-      return n;
-  }else{
-      return 0;
+  var n = parseInt(str)
+  if (n) {
+    if (n < -2147483648) return -2147483648
+    if (n > 2147483647) return 2147483647
+    return n
+  } else {
+    return 0
   }
-};
+}
