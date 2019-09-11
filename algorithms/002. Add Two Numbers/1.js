e77fd3ca67d9c2f17665462c36d76428b19020f3
znnnnn <846179626@qq.com>
2019-09-11 21:19:50 +0800

    update: 002 format code

--- algorithms/002. Add Two Numbers/1.js
+++ algorithms/002. Add Two Numbers/1.js
@@ -4,21 +4,21 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let curEle = result = new ListNode(0)
-    let carry = 0
-    while (l1 || l2 || carry) {
-        let sum = carry
-        if (l1) {
-            sum += l1.val
-            l1 = l1.next
-        }
-        if (l2) {
-            sum += l2.val
-            l2 = l2.next
-        }
-        carry = sum > 9 ? 1 : 0
-        curEle.next = new ListNode((sum) % 10)
-        curEle = curEle.next
-    }
-    return result.next
-};
+  let curEle = (result = new ListNode(0))
+  let carry = 0
+  while (l1 || l2 || carry) {
+    let sum = carry
+    if (l1) {
+      sum += l1.val
+      l1 = l1.next
+    }
+    if (l2) {
+      sum += l2.val
+      l2 = l2.next
+    }
+    carry = sum > 9 ? 1 : 0
+    curEle.next = new ListNode(sum % 10)
+    curEle = curEle.next
+  }
+  return result.next
+}
