b6f705e60f1bcb79f823fe21954150c039e5283e
znnnnn <846179626@qq.com>
2019-09-11 21:20:10 +0800

    update: 003 format code

--- algorithms/003. Longest Substring Without Repeating Characters/1.js
+++ algorithms/003. Longest Substring Without Repeating Characters/1.js
@@ -3,15 +3,16 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-  let max = 0, start = 0;
-  const map = new Map();
-  for(let i = 0; i < s.length; i++) {
-      const v = s[i];
-      if (map.has(v) && map.get(v) >= start) {
-          start = map.get(v) + 1;
-      }
-      map.set(v, i);
-      max = Math.max(max, i - start + 1);
+  let max = 0,
+    start = 0
+  const map = new Map()
+  for (let i = 0; i < s.length; i++) {
+    const v = s[i]
+    if (map.has(v) && map.get(v) >= start) {
+      start = map.get(v) + 1
+    }
+    map.set(v, i)
+    max = Math.max(max, i - start + 1)
   }
-  return max;
-};
+  return max
+}
