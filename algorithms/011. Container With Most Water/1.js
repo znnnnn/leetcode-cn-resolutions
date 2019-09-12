facc6149b15380ac65625e1831bf7e19c9f938fc
znnnnn <846179626@qq.com>
2019-09-12 22:45:11 +0800

    update: format code

--- algorithms/011. Container With Most Water/1.js
+++ algorithms/011. Container With Most Water/1.js
@@ -3,16 +3,15 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    var left = 0, right = height.length - 1;
-	var max = 0;
+  var left = 0,
+    right = height.length - 1
+  var max = 0
 
-	while (left < right) {
-		max = Math.max(max, Math.min(height[left], height[right]) * (right - left));
-		if (height[left] < height[right])
-			left++;
-		else
-			right--;
-	}
+  while (left < right) {
+    max = Math.max(max, Math.min(height[left], height[right]) * (right - left))
+    if (height[left] < height[right]) left++
+    else right--
+  }
 
-	return max;
-};
+  return max
+}
