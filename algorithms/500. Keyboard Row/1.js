a88962ab77fe63efc508fa1615f2894cc7efa215
znnnnn <846179626@qq.com>
2019-10-05 19:37:18 +0800

    update: format code

--- algorithms/500. Keyboard Row/1.js
+++ algorithms/500. Keyboard Row/1.js
@@ -2,9 +2,13 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var findWords = function (words) {
-    let result = words.filter(item => {
-        return !/[asdfghjklzxcvbnm]/i.test(item) || !/[qwertyuiopzxcvbnm]/i.test(item) || !/[qwertyuiopasdfghjkl]/i.test(item)
-    })
-    return result
+var findWords = function(words) {
+  let result = words.filter(item => {
+    return (
+      !/[asdfghjklzxcvbnm]/i.test(item) ||
+      !/[qwertyuiopzxcvbnm]/i.test(item) ||
+      !/[qwertyuiopasdfghjkl]/i.test(item)
+    )
+  })
+  return result
 }
