ba67de6266491af213520a9a4e585903fa872a61
znnnnn <846179626@qq.com>
2019-10-27 23:44:39 +0800

    update: format code

--- algorithms/017. Letter Combinations of a Phone Number/1.js
+++ algorithms/017. Letter Combinations of a Phone Number/1.js
@@ -3,26 +3,26 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-  if(digits.length === 0) return [];
+  if (digits.length === 0) return []
   const map = {
-      2: 'abc',
-      3: 'def',
-      4: 'ghi',
-      5: 'jkl',
-      6: 'mno',
-      7: 'prqs',
-      8: 'tuv',
-      9: 'wxyz'
-  };
-  let last = [''];
-  for (const num of digits) {
-      const current = [];
-      for (const item of last) {
-          for (const char of map[num]) {
-              current.push(`${item}${char}`);
-          }
-      }
-      last = current;
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'prqs',
+    8: 'tuv',
+    9: 'wxyz',
   }
-  return last;
-};
+  let last = ['']
+  for (const num of digits) {
+    const current = []
+    for (const item of last) {
+      for (const char of map[num]) {
+        current.push(`${item}${char}`)
+      }
+    }
+    last = current
+  }
+  return last
+}
