44367ea97da445fe77f85c06cdf79c835eea8fa9
znnnnn <846179626@qq.com>
2019-10-30 23:35:05 +0800

    update: format code

--- algorithms/026. Remove Duplicates from Sorted Array/26.js
+++ algorithms/026. Remove Duplicates from Sorted Array/26.js
@@ -1,12 +1,12 @@
 /*
- * @Description: 
- * @version: 
+ * @Description:
+ * @version:
  * @Author: znnnnn
  * @Date: 2019-09-14 22:51:06
  * @LastEditors: znnnnn
  * @LastEditTime: 2019-09-14 22:51:06
  */
-var removeDuplicates = function (nums) {    
+var removeDuplicates = function(nums) {
   nums.splice(0, nums.length, ...new Set(nums))
-  return nums.length   
-};
+  return nums.length
+}
